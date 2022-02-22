00e6494a3d1e2a7c86bc30f0e935b6e6b1258414
abdoelnaep <47692037+abdoelnaep@users.noreply.github.com>
2022-02-22 21:34:34 +0200

    my commit

---
+++ AngelaCard/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+ 
+//
+//  Created by Abdullah Mahmoud on 22/02/2022.
+
+  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(red:0.09, green:0.63, blue:0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                Image("abdullah")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 4)
+                )
+                Text("Abdullah Mahmoud")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+20 01024675159", imageName: "envelope.fill")
+                InfoView(text: "Abdoelnaep@gmail.com", imageName: "envelope.fill")
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
